4e79c1f59d407c6e3479a66c79cb28c16630fffb
van17071999-sys <van17071999@gmail.com>
2026-08-13 16:32:09 +0700

    Add flute notation detail route

---
+++ app/cam-am/[slug]/page.tsx
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+import { FluteDetail } from "../../cms-content-pages";
+
+export const metadata: Metadata = { title: "Bài cảm âm sáo trúc" };
+
+export default function Page() { return <FluteDetail />; }
