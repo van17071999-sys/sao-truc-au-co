9ebc948590bb78235b8cbd048da71e08242383fc
van17071999-sys <van17071999@gmail.com>
2026-08-20 11:07:14 +0700

    Update news detail page

--- app/tin-tuc/[slug]/page.tsx
+++ app/tin-tuc/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { NewsDetail } from "../../cms-content-pages";
 
-export const metadata: Metadata = { title: "Bài viết" };
+export const metadata: Metadata = {
+  title: "Bài viết | Sáo Trúc Âu Cơ",
+};
 
-export default function Page() { return <NewsDetail />; }
+export default function Page() {
+  return <NewsDetail />;
+}
